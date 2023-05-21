158515d02c9651e05f75436615c7e571dd18a7e6
jeffriardian <60500344+jeffriardian@users.noreply.github.com>
2023-05-22 02:18:39 +0700

    init2

---
+++ CodingCompetency4/CodingCompetency4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBar
+{
+    class myClass
+    {
+        public static void AddRule(int input, string output)
+        {
+            for (var n = 1; n <= 30; n++)
+            {
+                var isDivisible = false;
+
+                if ((n % input) == 0)
+                {
+                    Console.Write(output);
+                    isDivisible = true;
+                }
+
+                if (isDivisible)
+                    Console.WriteLine();
+                else
+                    Console.WriteLine(n);
+            }
+        }
+    }
+
+    class Foobar
+    {
+        static void Main()
+        {
+            Console.WriteLine("Silahkan masukan input rule yang diinginkan :");
+            var input = Console.ReadLine();
+            Console.WriteLine("Silahkan masukan output rule yang diinginkan :");
+            var output = Console.ReadLine();
+            myClass.AddRule(int.Parse(input), output);
+            Console.ReadLine();
+        }
+    }
+}
